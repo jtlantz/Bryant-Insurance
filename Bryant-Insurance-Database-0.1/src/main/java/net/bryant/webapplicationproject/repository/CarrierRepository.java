e6c989fd8b4ff6ac18d7c1e25e0fd2eecbd1a0fd
jtlantz <68471166+jtlantz@users.noreply.github.com>
2021-07-20 16:37:13 +0700

    Revert "Added SimpleResponseDTO"
    This reverts commit 9205725ffed2a9f5c065a0ab02c729987274b543.

--- Bryant-Insurance-Database-0.1/src/main/java/net/bryant/webapplicationproject/repository/CarrierRepository.java
+++
@@ -1,7 +0,0 @@
-package net.bryant.webapplicationproject.repository;
-
-import net.bryant.webapplicationproject.model.Carrier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrierRepository extends JpaRepository<Carrier, Long> {
-}
