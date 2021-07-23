6ffa70b4e5e6664dacc6849dda664a331596fd21
nannn16 <nannnyy16@gmail.com>
2021-07-23 09:50:58 +0700

    fix some bug

--- src/main/java/org/bryantinsurance/UserController.java
+++ src/main/java/org/bryantinsurance/UserController.java
@@ -7,7 +7,6 @@ import org.bryantinsurance.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
